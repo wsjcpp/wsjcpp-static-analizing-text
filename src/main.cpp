3ae30ade8ac34e7917d8ee39d9df1517a3cd9bec
Evgenii Sopov <mrseakg@gmail.com>
2020-10-20 23:11:19 +0700

    Init wsjcpp

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <string.h>
+#include <iostream>
+#include <algorithm>
+
+int main(int argc, const char* argv[]) {
+    std::string TAG = "MAIN";
+    std::string appName = std::string(WSJCPP_APP_NAME);
+    std::string appVersion = std::string(WSJCPP_APP_VERSION);
+
+    // TODO 
+
+    return 0;
+}
+
